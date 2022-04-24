d55394154d6913b8a2cc45e4b6c31ea0fc9cb3ec
diegofernandesss <fernandes.diego@academico.ifpb.edu.br>
2022-04-24 18:16:38 -0300

    carteirinha_nlw

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Carteirinha nlw 
+
+<h2>Principal parte do projeto:</h2>
+
+<ul>
+    <li>Utilização da API do Git Hub</li>
+    <li>Conceitos de css, html e javascript</li>
+    <li>Utilização do figma para a produzição sites</li>
+</ul>
---
+++ javascript.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia= {
+    github: "diegofernandesss",
+    /*youtube: "#",*/
+    facebook: "diegodacosta.fernandes.7",
+    instagram: "d.iegof"
+    /*twitter: "#"*/
+}
+
+const changeSocialMediaLinks = () => {
+    for(let li of socialLinks.children){
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+const getGitHubProfileInfos = () => {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
